f0a37d5b3bd9620f1d87951046e844670fd1f809
Salehi <apple@Apples-MacBook-Air.local>
2024-02-21 23:18:17 +0330

    Implement the structure

---
+++ src/App.tsx
@@ -0,0 +1,36 @@
+import React from "react";
+
+import "./App.css";
+
+function App() {
+  return (
+    <div className="TodoWrapper">
+      <h1>Todo App</h1>
+      {/* Add New Todo Form */}
+
+      <form className="TodoForm">
+        <input
+          type="text"
+          className="todo-input"
+          placeholder="add todo..."
+        />
+        <button type="submit" className="todo-btn">
+          Add Task
+        </button>
+      </form>
+
+      {/* display todos */}
+      <div className="Todo">
+        <p
+          className="" // or completed className
+        >
+          Test Todo Title :)
+        </p>
+        {/* icon={faTrash} */}
+        <div>{/* <FontAwesomeIcon /> */}</div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
